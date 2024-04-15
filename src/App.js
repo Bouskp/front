f3e0334834c8c8cc451a4bed0c7a9a34a5a28fe9
booskp <kouakouderek132@gmail.com>
2024-04-15 15:14:17 +0000

    premier push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
+import './props.css'
+import router from './router'
+import AppContext from './Appcontext'
+import axiosInstance from './axios'
+import { useDispatch } from 'react-redux'
+import { getUser } from './redux/slices/userSlice'
 
 function App() {
+  const [id, setId] = useState()
+  const dispatch = useDispatch()
+  dispatch(getUser(id))
+
+  useEffect(() => {
+    const getId = async () => {
+      try {
+        const res = await axiosInstance.get('/jwtid', { withCredentials: true })
+        setId(res.data.id)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getId()
+  }, [id])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContext.Provider value={{ id }}>
+      <RouterProvider router={router} />
+    </AppContext.Provider>
+  )
 }
 
-export default App;
+export default App
